f93f91e0c9a114b576e706dd40266fcb84f7365f
shafiul-naim <shafiul.naim@gmail.com>
2022-04-19 16:27:29 +0600

    about section

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import about from "../../images/about.jpg";
 
 const About = () => {
-    return (
-        <div>
-            <h2>This is about us</h2>
+  return (
+    <div>
+      <div className="card mb-3 w-100  my-4">
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src={about} className="img-fluid rounded-start" alt="..." />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title text-info">About me</h5>
+              <p className="card-text fs-3">
+                I am a professional sports photographer.I love to take picture of beautiful moment of the game you are playing.If you want to make a memento,caught the excitement of the game then i am here for you.
+              </p>
+              
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-       <footer className='container-fluid mt-4 bg-dark d-flex justify-content-center align-items-center' style={{height: '80px'}}>
+       <footer className=' container-fluid mt-4 bg-dark d-flex justify-content-center align-items-center' style={{height: '80px', marginTop: '-80px'}}>
            <p className='text-center mx-auto text-white '><small > &copy; All right reserved to SPhotos</small></p>
        </footer>
     );

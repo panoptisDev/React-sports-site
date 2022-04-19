19651a469c5296eb386034af1f0ef4f3a327c42e
shafiul-naim <shafiul.naim@gmail.com>
2022-04-19 19:11:56 +0600

    404 page modified

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -3,9 +3,9 @@ import error from '../../../images/404.jpg'
 
 const NotFound = () => {
     return (
-        <div>
+        <div className='w-75 mx-auto'>
         <h2 className='text-primary text-center'>Sorry page not found</h2>
-        <img className='img-fluid' src={error} alt="" />
+        <img  className='img-fluid' src={error} alt="" />
     </div>
     );
 };

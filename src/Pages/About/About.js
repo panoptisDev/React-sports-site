551295122f1b9d3a06e50455f16e4e11c6513dca
shafiul-naim <shafiul.naim@gmail.com>
2022-04-19 20:32:25 +0600

    about me added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -11,11 +11,15 @@ const About = () => {
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <h5 className="card-title text-info">About me</h5>
+              <h3 className="card-title text-secondary">Shafiul Islam</h3>
               <p className="card-text fs-4">
-                I am a professional sports photographer.I love to take picture of beautiful moment of the game you are playing.It was my passion from childhood since i love sports and love every moment of it.Thats why i wanted to make those moment memorable through my lens.I worked hard for that. And still working hard to make it big thing for my life.
+                I am a professional sports photographer.I love to take picture
+                of beautiful moment of the game you are playing.It was my
+                passion from childhood since i love sports and love every moment
+                of it.Thats why i wanted to make those moment memorable through
+                my lens.I worked hard for that. And still working hard to make
+                it big thing for my life.
               </p>
-              
             </div>
           </div>
         </div>
